1c3446abdffa3cd66cb22c99962efe49188c4705
wakoTheDev <wakopaul19@gmail.com>
2025-10-21 15:12:29 +0300

    registered the stalk app in the settings file, registered the templates folder in the settings file and creating the templates folder

--- pharmacy/pharmacy/settings.py
+++ pharmacy/pharmacy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pwo85*rw(2on!mf3@cvn+4g!=sq+lez^c_=qdn&pe(!v3_@_xu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stalk',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'pharmacy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
