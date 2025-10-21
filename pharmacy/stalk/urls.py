3a5e0964041b48dac03e2bc6df8a56364768f9cb
wakoTheDev <wakopaul19@gmail.com>
2025-10-21 15:08:28 +0300

    created and updated the url file in the stalk app

---
+++ pharmacy/stalk/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+import views
+
+urlpatterns = [
+    path('', views.medicine_list,name="medicine_list"),
+    path('add/', views.add_medicine,name="add_medicine"),
+    path('update/<int:medicine_id>/', views.update_medicine,name="update_medicine"),
+    path('delete/<int:medicine_id>/', views.delete_medicine,name="delete_medicine"),
+]
